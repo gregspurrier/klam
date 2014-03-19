604fd30cc41052980a8b97fde659a252a93de344
Greg Spurrier <greg@sourcematters.org>
2014-03-19 07:49:38 -0700

    Extract functional spec helpers

--- spec/functional/atoms_spec.rb
+++ spec/functional/atoms_spec.rb
@@ -1,34 +1,19 @@
 require 'spec_helper'
 
-describe 'Atoms' do
-  def read_kl(str)
-    stream = StringIO.new(str)
-    reader = Klam::Reader.new(stream)
-    reader.next
-  end
-
-  def expect_eval_kl(str)
-    form = read_kl(str)
-    expect(@env.__send__(:"eval-kl", form))
-  end
-
-  before(:each) do
-    @env = Klam::Environment.new
-  end
-
+describe 'Atoms', :type => :functional do
   describe 'numbers' do
     it 'integers evaluate to themselves' do
-      expect_eval_kl('37').to eq(read_kl('37'))
+      expect_kl('37').to eq(read_kl('37'))
     end
 
     it 'reals evaluate to themselves' do
-      expect_eval_kl('37.42').to eq(read_kl('37.42'))
+      expect_kl('37.42').to eq(read_kl('37.42'))
     end
   end
 
   describe 'symbols' do
     it 'evaluate to themselves' do
-      expect_eval_kl('sym').to eq(read_kl('sym'))
+      expect_kl('sym').to eq(read_kl('sym'))
     end
 
     it 'supports the full range of initial symbol characters' do
@@ -38,28 +23,28 @@ describe 'Atoms' do
       all_chars += "=-*/+_?$!@~.><&%'#`;:{}"
 
       all_chars.each_char do |c|
-        expect_eval_kl(c).to eq(read_kl(c))
+        expect_kl(c).to eq(read_kl(c))
       end
     end
   end
 
   describe 'strings' do
     it 'evaluate to themselves' do
-      expect_eval_kl('"a string"').to eq(read_kl('"a string"'))
+      expect_kl('"a string"').to eq(read_kl('"a string"'))
     end
 
     it 'supports embedded single quotes' do
-      expect_eval_kl('"\'quote\'"').to eq(read_kl('"\'quote\'"'))
+      expect_kl('"\'quote\'"').to eq(read_kl('"\'quote\'"'))
     end
   end
 
   describe 'booleans' do
     it 'evaluates true to itself' do
-      expect_eval_kl('true').to eq(read_kl('true'))
+      expect_kl('true').to eq(read_kl('true'))
     end
 
     it 'evaluates false to itself' do
-      expect_eval_kl('false').to eq(read_kl('false'))
+      expect_kl('false').to eq(read_kl('false'))
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,27 @@
+require 'stringio'
 require 'klam'
 
-require 'stringio'
+module EvalKl
+  def read_kl(str)
+    stream = StringIO.new(str)
+    reader = Klam::Reader.new(stream)
+    reader.next
+  end
+
+  def eval_kl(str)
+    form = read_kl(str)
+    @env.__send__(:"eval-kl", form)
+  end
+
+  def expect_kl(str)
+    expect(eval_kl(str))
+  end
+end
+
+RSpec.configure do |cfg|
+  # Support eval_kl and friends in functional specs
+  cfg.include EvalKl, :type => :functional
+  cfg.before(:each, :type => :functional) do
+    @env = Klam::Environment.new
+  end
+end
