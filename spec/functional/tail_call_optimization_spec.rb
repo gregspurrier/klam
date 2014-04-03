d4294fd71cb3b6eae363edc32c5a20dfa0309f56
Greg Spurrier <greg@sourcematters.org>
2014-04-02 20:17:44 -0700

    Add more TCO tests

--- spec/functional/tail_call_optimization_spec.rb
+++ spec/functional/tail_call_optimization_spec.rb
@@ -16,6 +16,16 @@ describe 'Tail call optimization', :type => :functional do
     expect_kl('(count-down 10000)').to be(true)
   end
 
+  it 'uses the current values of the params when calculating the new ones' do
+    eval_kl <<-EOKL
+      (defun fact-iter (N Accum)
+        (if (= N 1)
+          Accum
+          (fact-iter (- N 1) (* N Accum))))
+    EOKL
+    expect_kl('(fact-iter 5 1)').to eq(120)
+  end
+
   it 'preserves the value of closed-over loop variables' do
     eval_kl <<-EOKL
       (defun sample-loop-val (N Closure)
@@ -24,5 +34,15 @@ describe 'Tail call optimization', :type => :functional do
           Closure))
     EOKL
     expect_kl('((sample-loop-val 2 foo) ignored)').to eq(1)
+  end
+
+  it 'preserves the value of frozen loop variables' do
+    eval_kl <<-EOKL
+      (defun sample-frozen-val (N Thunk)
+        (if (> N 0)
+          (sample-frozen-val (- N 1) (freeze N))
+          Thunk))
+    EOKL
+    expect_kl('((sample-frozen-val 2 foo))').to eq(1)
   end
 end
