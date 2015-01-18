cb0d1004477510bc47ca345169c211cf8e307e0f
Greg Spurrier <greg@sourcematters.org>
2015-01-17 21:13:16 -0800

    Fix rb-const in Ruby 1.9.3

--- lib/klam/primitives/interop.rb
+++ lib/klam/primitives/interop.rb
@@ -9,9 +9,20 @@ module Klam
       alias_method :"rb-send", :rb_send
       remove_method :rb_send
 
-      def rb_const(name)
-        ::Module.const_get(name)
+      if RUBY_ENGINE == 'ruby' && RUBY_VERSION < '2'
+        def rb_const(name)
+          parts = name.to_s.split('::')
+          parts.shift if parts.first.empty?
+          parts.reduce(::Module) do |m, x|
+            m.const_get(x)
+          end
+        end
+      else
+        def rb_const(name)
+          ::Module.const_get(name)
+        end
       end
+
       alias_method :"rb-const", :rb_const
       remove_method :rb_const
     end
