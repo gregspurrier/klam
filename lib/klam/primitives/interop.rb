b5d447335315d42b1b78dd0deae33bccdbd23dd9
Greg Spurrier <greg@sourcematters.org>
2015-01-29 06:12:44 -0800

    Add rb-send-block

--- lib/klam/primitives/interop.rb
+++ lib/klam/primitives/interop.rb
@@ -9,6 +9,20 @@ module Klam
       alias_method :"rb-send", :rb_send
       remove_method :rb_send
 
+      def rb_send_block(obj, method_name, blk_arity, blk, *args)
+        if blk.instance_of?(::Symbol)
+          # The caller won't take advantage of the currying, but we already
+          # are tracking the curried form. This also allows for paritial
+          # application of the named function, which could be interesting.
+          blk = @curried_methods[blk]
+        else
+          blk = ::Klam::Primitives::Interop.uncurry(blk, blk_arity)
+        end
+        obj.send(method_name, *args, &blk)
+      end
+      alias_method :"rb-send-block", :rb_send_block
+      remove_method :rb_send_block
+
       if RUBY_VERSION < '2.'
         def rb_const(name)
           parts = name.to_s.split('::')
@@ -25,6 +39,47 @@ module Klam
 
       alias_method :"rb-const", :rb_const
       remove_method :rb_const
+
+      class << self
+        def uncurry(blk, blk_arity)
+          case blk_arity
+          when 0
+            uncurry0(blk)
+          when 1
+            blk
+          when 2
+            uncurry2(blk)
+          when 3
+            uncurry2(blk)
+          when 4
+            uncurry2(blk)
+          when 5
+            uncurry2(blk)
+          else
+            ::Kernel.raise ::Klam::Error, "unsupported arity: #{blk_arity}"
+          end
+        end
+
+        def uncurry0(blk)
+          -> { blk.call(:NIL) }
+        end
+
+        def uncurry2(blk)
+          -> a, b { blk.call(a).call(b) }
+        end
+
+        def uncurry3(blk)
+          -> a, b, c { blk.call(a).call(b).call(c) }
+        end
+
+        def uncurry4(blk)
+          -> a, b, c, d { blk.call(a).call(b).call(c).call(d) }
+        end
+
+        def uncurry5(blk)
+          -> a, b, c, d, e { blk.call(a).call(b).call(c).call(d).call(e) }
+        end
+      end
     end
   end
 end
--- spec/functional/extensions/interop_spec.rb
+++ spec/functional/extensions/interop_spec.rb
@@ -7,6 +7,29 @@ describe 'extension: Ruby interop primitives', :type => :functional do
     end
   end
 
+  describe 'rb-send-block' do
+    it 'invokes a method on a Ruby object, passing the provided block' do
+      code = '(rb-send-block (cons 1 (cons 2 ())) map 1 (lambda X (* 2 X)))'
+      expect_kl(code).to eq([2, 4])
+    end
+
+    it 'supports blocks with arity greater than 1' do
+      eval_kl('(set vec (absvector 2))')
+      result = eval_kl <<-EOKL
+        (let V (absvector 2)
+          (do (rb-send-block (cons 1 (cons 2 ())) each_with_index 2
+                             (lambda X (lambda Y (address-> V Y X))))
+              V))
+      EOKL
+      expect(result).to eq([1, 2])
+    end
+
+    it 'supports symbols as blocks' do
+      eval_kl('(set vec (absvector 2))')
+      expect_kl('(rb-send-block (cons 1 (cons 2())) map 1 str)').to eq(%w{1 2})
+    end
+  end
+
   describe 'rb-const' do
     it 'looks up the named constant in the Ruby environment' do
       expect_kl('(rb-const "::Math::PI")').to eq(Math::PI)
